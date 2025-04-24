5b813bcbca82d6e6a0696fd01daf3abbc8d2a0c2
unknown <dhercan391@gmail.com>
2025-04-24 10:20:32 +0200

    pantalla login funcionando con consulta a bbdd

--- BackOfficeSpring/src/main/java/com/backoffice/BackOfficeSpring/login/AuthController.java
+++ BackOfficeSpring/src/main/java/com/backoffice/BackOfficeSpring/login/AuthController.java
@@ -7,23 +7,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
-
 @RequestMapping("/api")
-@CrossOrigin(origins = "*") // permite peticiones desde Angular
 public class AuthController {
 
     @Autowired
     private Usuariorepository usuarioRepository;
 
     @PostMapping("/login") 
-
     public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest loginRequest) {
         Optional<Usuario> usuario = usuarioRepository.findByNombreAndContrasena(
             loginRequest.getNombre(), loginRequest.getContrasena());
@@ -33,4 +30,12 @@ public class AuthController {
 
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/prueba")
+    public ResponseEntity<Map<String, String>> pruebaEndpoint() {
+        Map<String, String> response = new HashMap<>();
+        response.put("mensaje", "Este es un texto plano de prueba");
+        return ResponseEntity.ok(response);
+    }
 }
+
