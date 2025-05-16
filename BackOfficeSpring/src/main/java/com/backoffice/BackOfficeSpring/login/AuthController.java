acd406bf5355656e10af57ee958e9424376db6cb
Laura Marcos <lauramarcosmo@gmail.com>
2025-05-16 12:26:22 +0200

    pintando copias de seguridad

--- BackOfficeSpring/src/main/java/com/backoffice/BackOfficeSpring/login/AuthController.java
+++ BackOfficeSpring/src/main/java/com/backoffice/BackOfficeSpring/login/AuthController.java
@@ -1,6 +1,7 @@
 package com.backoffice.BackOfficeSpring.login;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -31,12 +32,13 @@ public class AuthController {
     @PostMapping("/login") 
     public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest loginRequest) {
        
+        // Buscamos en la base de datos por lo introducido en el login (recuperamos todos los campos )
             Optional<Usuario> usuarioPorNombre = usuarioRepository.findByNombre(loginRequest.getNombre());
 
         Map<String, Object> response = new HashMap<>();
-        //response.put("success", usuario.isPresent());
+        
         if (usuarioPorNombre.isPresent()) {
-            Usuario usuarioBD = usuarioPorNombre.get();
+            Usuario usuarioBD = usuarioPorNombre.get(); //obtenemos todos los campos 
             boolean passwordMatch = encoder.matches(loginRequest.getContrasena(), usuarioBD.getContrasena());
 
             if (passwordMatch){
@@ -81,64 +83,41 @@ public class AuthController {
         public ResponseEntity<String> generarCopiaSeguridad(@RequestParam("id") String idCliente) {
             String url = "http://backoffice.practicas/awj-back/backoffice/api/genera_copia_seguridad?id_cliente=" + idCliente;
             BackupRequest respuesta;
+             
             try{
-            RestTemplate restTemplate = new RestTemplate();
-            String rawJson = restTemplate.getForObject(url, String.class);
-            System.out.println("JSON recibido: " + rawJson);
-            respuesta= restTemplate.getForObject(url, BackupRequest.class); 
-            Backup backup = new Backup();
-            backup.setCliente(respuesta.getCliente());
-            backup.setFechaHora(respuesta.getFechaHora());
-            backup.setDescripcion(respuesta.getDescripcion());
-            backupRepository.save(backup);    
-        } catch (Exception e) {
-            System.err.println("Error al obtener datos: " + e.getMessage());
-            return ResponseEntity.status(500).body("Error al generar copia: " + e.getMessage());
+                RestTemplate restTemplate = new RestTemplate();
+                String rawJson = restTemplate.getForObject(url, String.class);
+                System.out.println("JSON recibido: " + rawJson);
+
+                //Obtenemos la respuesta parseada como objeto tipo backup para luego obtener sus campos 
+                respuesta= restTemplate.getForObject(url, BackupRequest.class); 
+                Backup backup = new Backup();
+                backup.setCliente(respuesta.getCliente());
+                backup.setFechaHora(respuesta.getFechaHora());
+                backup.setDescripcion(respuesta.getDescripcion());
+                backupRepository.save(backup);    //guardamos en base de datos
+
+            } catch (Exception e) {
+                System.err.println("Error al obtener datos: " + e.getMessage());
+                return ResponseEntity.status(500).body("Error al generar copia: " + e.getMessage());
             }
             return ResponseEntity.ok("Copia de seguridad generada y guardada correctamente.");
     }
 
-        // @PostMapping("/obtenerCopiasSeguridad")
-        // public ResponseEntity<String> guardarBackup(@RequestBody BackupRequest request) {
-        //     Backup backup = new Backup();
-        //     backup.setCliente(request.getCliente());
-        //     backup.setFechaHora(request.getFechaHora());
-        //     backup.setDescripcion(request.getDescripcion());
 
-        //     backupRepository.save(backup);
-
-        //     return ResponseEntity.ok("Backup guardado correctamente");
-        // }
-
-        
+    @GetMapping("/backups")
+    public ResponseEntity<List<Backup>> getAllBackups() {
+        List<Backup> backups = backupRepository.findAll();
+        return ResponseEntity.ok(backups);
+    }
 }
 
-    /*
-     * Antes de eliminar findByNombreAndContrasena
-     *     @PostMapping("/login") 
-    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest loginRequest) {
-        Optional<Usuario> usuario = usuarioRepository.findByNombreAndContrasena(
-            loginRequest.getNombre(), encoder.encode(loginRequest.getContrasena()));
-            System.out.println("****PASS ORIGINAL: "+ loginRequest.getContrasena());
-            System.out.println("****PAss HASHEADA: " + encoder.encode(loginRequest.getContrasena()));
-        
-            Optional<Usuario> usuarioPorNombre = usuarioRepository.findByNombre(loginRequest.getNombre());
-        
-        Map<String, Object> response = new HashMap<>();
-        response.put("success", usuario.isPresent());
-        if (!usuario.isPresent() && usuarioPorNombre.isEmpty() ) {
-            response.put("errorMsg", "Usuario no encontrado");
-        }else{
-            response.put("errorMsg", "Contraseña incorrecta");
-        }
-            
-        
-        response.put("errorMessage", usuario.isPresent());
-        return ResponseEntity.ok(response);
-    }
-     * 
-     * 
-     */
+
+
+
+
+
+      
 
     
 
--- BackOfficeSpring/src/main/java/com/backoffice/BackOfficeSpring/login/BackupRepository.java
+++ BackOfficeSpring/src/main/java/com/backoffice/BackOfficeSpring/login/BackupRepository.java
@@ -1,5 +1,6 @@
 package com.backoffice.BackOfficeSpring.login;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
